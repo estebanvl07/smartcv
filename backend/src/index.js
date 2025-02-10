1d46b78af24cebcfc48ecd4beb5f75b4f430901a
esteban_vl <estebanvl07@github.com>
2025-02-10 08:35:40 -0500

    ADD: backend structure

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("¡Hola, Express!");
+});
+
+// Iniciar el servidor en el puerto 3000
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
