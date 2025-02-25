d16d5291fc99d6f8cf876f5364d60670c0833828
KIy077 <thebinary0101@gmail.com>
2025-02-25 15:12:14 -0500

    Crud complete

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
-import { getUserById } from "../controllers/user.controller.js";
+import { 
+    getUserById,
+    getAllUsers,
+    createUsers,
+    updateUsers,
+    deleteUsers
+ } from "../controllers/user.controller.js";
 
 const router = Router();
 
+
+// Obtiene un usuario por id
 router.get("/:id", getUserById);
+
+// Obtener todos los usuarios
+router.get("/", getAllUsers);
+
+// Crear Usuarios
+router.post("/", createUsers);
+
+// Actualizar usuarios por id
+router.put("/:id", updateUsers)
+
+// Eliminar usuaros
+router.delete("/:id", deleteUsers)
+
 
 export default router;
