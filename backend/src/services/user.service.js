71bbde0eff1dfd93b3a433af8c830d30cce1be66
KIy077 <thebinary0101@gmail.com>
2025-02-26 09:21:19 -0500

    CRUD Services

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -7,8 +7,64 @@ export const getUserById = async (id) => {
         id,
       },
     });
+
+    if(!userFound){
+      throw new Error("User not found")
+    }
+
     return userFound;
   } catch (error) {
     throw error;
   }
 };
+
+export const getAllUsers = async()=>{
+  try{
+    const users = await prisma.user.findMany({
+      include: {
+        accounts: true,
+        cvs: true,
+      }
+    });
+    return users;
+  }catch(error){
+    throw new Error("Error getting users");
+  }
+}
+
+
+export const createUser = async(userData) => {
+  try{
+    const newUser = await prisma.user.create({
+      data: userData,
+    });
+    return newUser;
+  }catch(error){
+    throw new Error("Error creating the user")
+  }
+}
+
+export const updateUser = async()=>{
+  try{
+    const updatedUser = await prisma.user.update({
+      where: {id},
+      data: userData,
+    });
+    return updatedUser;
+  }catch(error){
+    throw new Error("Error updating user");
+  }
+}
+
+
+export const deleteUser = async()=>{
+  try{
+    const deletedUser = await prisma.user.delete({
+      where: {id},
+    });
+    return {message:"User successfully deleted"};
+  }catch(error){
+    throw new Error("Error deleting user");
+    
+  }
+}
